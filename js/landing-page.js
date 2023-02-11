6152ea76163efbba1c9ad17761b3ea2973a0a1df
sakib <s.sarder232002@gmail.com>
2023-02-12 04:52:22 +0600

    landing page done

---
+++ js/landing-page.js
@@ -0,0 +1,15 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    const userEmail = document.getElementById('user-email');
+    const inputEmailField = userEmail.value;
+
+    const userPassword = document.getElementById('user-password');
+    const inputPasswordField = userPassword.value;
+    console.log(inputEmailField, inputPasswordField);
+
+    if (inputEmailField === 'sakib@babar-bank.com' && inputPasswordField === 'helloWorld') {
+        window.location.href = 'bank.html'
+    }
+    else {
+        alert('Oi beta email password thik moto de')
+    }
+})
